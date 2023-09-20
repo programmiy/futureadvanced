2f608a5f2f30cbb024906cc39aeb76d2defce3ef
programmiy <yyh4814@kakao.com>
2023-09-20 17:49:53 +0900

    queue practice end

--- practice/priority queue.py
+++ practice/priority queue.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue
-#1. slow but safe
+#1. slow but safe    max-hip
 pq = PriorityQueue()
 
 pq.put(123)
@@ -11,5 +11,19 @@ while not pq.empty():
 
 
 import heapq
-#2.   fast but n-safe
+#2.   fast but n-safe   min-heap
+
+h = []
+
+heapq.heappush(h ,123)
+
+
+heapq.heappush(h ,456)
+
+
+heapq.heappush(h ,789)
+while len(h):
+    print(heapq.heappop(h))
+
+
 
