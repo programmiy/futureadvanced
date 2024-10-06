c40989628e790a0f75467501b78f217e391f29ae
programmiy <yyh4814@kakao.com>
2024-10-06 17:26:52 +0900

    ss

--- try c#/Access Modifiers vasic/obj/Debug/net8.0/Access Modifiers vasic.AssemblyInfo.cs
+++ try c#/Access Modifiers vasic/obj/Debug/net8.0/Access Modifiers vasic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Access Modifiers vasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479dd2877ee5c0f59fe0048b60c960ce486b738b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa68e37dc6f9c776c60f558e02fa2b8a66b448e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Access Modifiers vasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Access Modifiers vasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
