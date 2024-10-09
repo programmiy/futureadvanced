3dbaea409749bd542e2efc66738ccd9018d56ade
programmiy <yyh4814@kakao.com>
2024-10-09 17:03:50 +0900

    react update

---
+++ React/jsx.js
@@ -0,0 +1,9 @@
+const heading = <h1>Mozilla Developer Network</h1>;
+
+
+const header = React.createElement(
+    "header",
+    null,
+    React.createElement("h1", null, "Mozilla Developer Network"),
+);
+  
